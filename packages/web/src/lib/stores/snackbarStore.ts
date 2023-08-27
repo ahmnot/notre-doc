b00bb2260fb2a1b4b8b07c3b53f1092025eecdae
Othman MOATASSIME <othmanmoat@hotmail.fr>
2023-08-27 23:23:47 +0200

    cleanup + added snackbars

--- packages/web/src/lib/stores/snackbarStore.ts
+++
@@ -1,6 +0,0 @@
-import type Snackbar from "@smui/snackbar"
-import { writable } from "svelte/store"
-
-export let snackbarLabel = writable('Résultat indéterminé.');
-export let snackbarClass = writable('demo-warning');
-
--- packages/web/src/routes/rendez-vous/+page.svelte
+++ packages/web/src/routes/rendez-vous/+page.svelte
@@ -50,7 +50,9 @@
 
 	let loading = false;
 
-	let colorError = 'color : #b71c1c';
+	let inputClasses = 'field label border';
+	let snackbarClass = 'snackbar error';
+	let snackbarMessage = "Échec."
 
 	/**
 	 * this does something when the form submits.
@@ -77,14 +79,10 @@
 					if (result.data && search === '?/final') {
 						await createPatient(result.data.data as PatientForm);
 
-						toast.success('Succès !', {
-							position: 'bottom-center',
-							style: 'border: 1px solid #6299D2; padding: 16px; color: #6299D2;',
-							iconTheme: {
-								primary: '#32CD32',
-								secondary: '#fff'
-							}
-						});
+						snackbarClass = 'snackbar success green active';
+						snackbarMessage = 'Succès !'
+
+						setTimeout(() => snackbarClass = 'snackbar error', 4000)
 
 						await update();
 					}
@@ -105,25 +103,16 @@
 					break;
 				case 'failure':
 				case 'error':
-					toast.error('Échec.', {
-						position: 'bottom-right',
-						style: 'border: 1px solid #6299D2; padding: 16px; color: #6299D2;',
-						iconTheme: {
-							primary: '#A52A2A',
-							secondary: '#fff'
-						}
-					});
+					snackbarClass = 'snackbar error active';
 
+					setTimeout(() => snackbarClass = 'snackbar error', 4000)
 					await update();
-
 					break;
 				default:
 					await update();
 			}
 		};
 	};
-
-	let inputClasses = 'field label border';
 </script>
 
 <article class="fill">
@@ -253,217 +242,6 @@
 	</form>
 </article>
 
-<!-- <div class="centered">
-	<Paper style="width: 940px; height: 600px;">
-		<Title>Prise de rendez-vous</Title>
-		<Subtitle><small style="opacity:0.45">Veuillez renseigner les champs suivants.</small></Subtitle
-		>
-		<Content>
-			<form method="POST" use:enhance={handleEnhance} novalidate>
-				<div>
-					<div style:display={formStep === 1 ? '' : 'none'}>
-						<div class="columns">
-							<div>
-								<Textfield
-									value={form?.data?.telephone ?? ''}
-									label="Téléphone"
-									input$name="telephone"
-									input$maxlength={18}
-									disabled={loading}
-									type="tel"
-									on:focus={() => (telephoneFocus = true)}
-								>
-									<svelte:fragment slot="helper">
-										{#if form?.errors?.telephone}
-											<HelperText persistent style={colorError}>
-												{#if !telephoneFocus}
-													{form?.errors?.telephone}
-												{/if}
-											</HelperText>
-										{:else}
-											<HelperText />
-										{/if}
-									</svelte:fragment>
-								</Textfield>
-							</div>
-						</div>
-						<div class="columns">
-							<div>
-								<Textfield
-									value={form?.data?.email ?? ''}
-									label="E-mail"
-									input$name="email"
-									type="email"
-									disabled={loading}
-									on:focus={() => (emailFocus = true)}
-								>
-									<svelte:fragment slot="helper">
-										{#if form?.errors?.email}
-											<HelperText persistent style={colorError}>
-												{#if !emailFocus}
-													{form?.errors?.email}
-												{/if}
-											</HelperText>
-										{:else}
-											<HelperText />
-										{/if}
-									</svelte:fragment>
-								</Textfield>
-							</div>
-						</div>
-
-						<div>&nbsp;</div>
-
-						<span class="right">
-							<Button
-								formaction="?/step1"
-								variant="raised"
-								color="secondary"
-								style="display: flex; justify-content: stretch;"
-							>
-								{#if !loading}
-									<Label>Suivant</Label>
-									<Icon
-										class="material-symbols-outlined"
-										style="font-variation-settings: 'FILL' 1, 'wght' 700, 'GRAD' 200, 'opsz' 40"
-										>arrow_forward</Icon
-									>
-								{:else}
-									<CircularProgress
-										class="my-colored-circle"
-										style="height: 24px; width: 92.15px;"
-										indeterminate
-									/>
-								{/if}
-							</Button>
-						</span>
-					</div>
-
-					<div style:display={formStep === 2 ? '' : 'none'}>
-						<div class="columns">
-							<div>
-								<Textfield
-									value={form?.data?.nom ?? ''}
-									label="Nom"
-									input$name="nom"
-									disabled={loading}
-									on:focusin={() => (nomFocus = true)}
-								>
-									<svelte:fragment slot="helper">
-										{#if form?.errors?.nom}
-											<HelperText persistent style={colorError}>
-												{#if !nomFocus}
-													{form?.errors?.nom}
-												{/if}
-											</HelperText>
-										{:else}
-											<HelperText>Nom de naissance</HelperText>
-										{/if}
-									</svelte:fragment>
-								</Textfield>
-							</div>
-						</div>
-						<div class="columns">
-							<div>
-								<Textfield
-									value={form?.data?.prenom ?? ''}
-									label="Prénom"
-									input$name="prenom"
-									disabled={loading}
-									on:focusin={() => (prenomFocus = true)}
-								>
-									<svelte:fragment slot="helper">
-										{#if form?.errors?.prenom}
-											<HelperText persistent style={colorError}>
-												{#if !prenomFocus}
-													{form?.errors?.prenom}
-												{/if}
-											</HelperText>
-										{:else}
-											<HelperText />
-										{/if}
-									</svelte:fragment>
-								</Textfield>
-							</div>
-						</div>
-						<div class="columns">
-							<div style="width: 182px">
-								<Textfield
-									value={form?.data?.dateNaissance ?? ''}
-									label="Date de naissance"
-									input$name="dateNaissance"
-									style="width: 100%"
-									helperLine$style="width: 100%"
-									type="date"
-									disabled={loading}
-									on:focusin={() => (dateNaissanceFocus = true)}
-								>
-									<svelte:fragment slot="helper">
-										{#if form?.errors?.dateNaissance}
-											<HelperText persistent style={colorError}>
-												{#if !dateNaissanceFocus}
-													{form?.errors?.dateNaissance}
-												{/if}
-											</HelperText>
-										{:else}
-											<HelperText />
-										{/if}
-									</svelte:fragment>
-								</Textfield>
-							</div>
-						</div>
-
-						<div>&nbsp;</div>
-
-						<div>
-							<span class="left">
-								<IconButton
-									type="button"
-									variant="outlined"
-									color="secondary"
-									style="font-variation-settings: 'FILL' 1, 'wght' 700, 'GRAD' 200, 'opsz' 40"
-									class="material-symbols-outlined"
-									size="button"
-									touch
-									on:click={() => (formStep = 1)}
-								>
-									arrow_back
-								</IconButton>
-							</span>
-							<span class="right">
-								<Button
-									formaction="?/final"
-									type="submit"
-									variant="raised"
-									aria-busy={loading}
-									color="secondary"
-									style="display: flex; justify-content: stretch;"
-								>
-									{#if !loading}
-										<Icon
-											class="material-symbols-outlined"
-											style="font-variation-settings: 'FILL' 1, 'wght' 700, 'GRAD' 200, 'opsz' 40"
-											>send</Icon
-										>
-										<Label>Envoyer</Label>
-									{:else}
-										<CircularProgress
-											class="my-colored-circle"
-											style="height: 24px; width: 92.15px;"
-											indeterminate
-										/>
-									{/if}
-								</Button>
-							</span>
-						</div>
-					</div>
-				</div>
-			</form>
-		</Content>
-	</Paper>
-</div> -->
-
-<br />
-
+<div class={snackbarClass}>{snackbarMessage}</div>
 <style>
 </style>
