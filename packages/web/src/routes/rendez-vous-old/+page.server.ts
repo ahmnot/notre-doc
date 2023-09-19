0ae96de779651027ce3fd8bef9beb49436e962de
ahmnot <othmanmoat@hotmail.fr>
2023-09-19 22:14:02 +0200

    small refactorings

--- packages/web/src/routes/rendez-vous-old/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { zodSchemaStep1, zodSchemaStep2, validate } from '$lib/validation'
-import type { Actions } from "@sveltejs/kit"
-
-export const actions: Actions = {
-    step1: validate(zodSchemaStep1),
-    final: validate({ ...zodSchemaStep1, ...zodSchemaStep2 }),
-}
-
